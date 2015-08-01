040e2a1f3e18b10e743e24658fdf24513093bf14
sabine <sabine@sabine-virtual-machine>
2015-08-01 13:14:43 +0200

    List with checkboxes added

---
+++ Syntheny_Scatterplot_SH_JS_RG/public_html/js/functions.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+function readJSON(dataset) {
+    /* Reads the given JSON file and pushes it to the named array
+     * returns array
+     * 
+     * dataset.push(new_arraypart);
+     * 
+     * Needed if more genomes are uploaded to keep the older ones.
+     * */
+    return dataset;
+}
