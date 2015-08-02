261eb6c431a58e0076a3f08b0d76bc1c9a83225d
sabine <sabine@sabine-virtual-machine>
2015-08-02 12:00:58 +0200

    separated code, getting selected genomes, comments

--- Syntheny_Scatterplot_SH_JS_RG/public_html/js/functions.js
+++ Syntheny_Scatterplot_SH_JS_RG/public_html/js/functions.js
@@ -1,18 +1,44 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * Collection of functions used in the project.
+ * File will be loaded in <head> of the "index.html".
  */
 
 
-
-function readJSON(dataset) {
-    /* Reads the given JSON file and pushes it to the named array
-     * returns array
-     * 
-     * dataset.push(new_arraypart);
-     * 
-     * Needed if more genomes are uploaded to keep the older ones.
-     * */
+/* Reads the given JSON file and pushes it to the given array.
+ * Needed if more genomes are uploaded to keep the older ones.
+ * 
+ * @param {Array} dataset
+ * @param {JSON-File} json
+ * @returns {Array}
+ */
+function readJSON(dataset, json) {
+    // dataset.push(new_arraypart);
     return dataset;
+}
+
+
+/* Fetches selection of genomes from inputs "genomeSelect".
+ * 
+ * @param {Array} dataset
+ * @returns {Array} scatterplot;
+ * 
+ * TBD:
+ * Saves each checked input. Pairwise comparison for homologouse genes.
+ *  
+ * Known Bugs:
+ * First click deletes, second works, same behaviour after editing selection
+ * Check the form/inputs/buttons
+ */
+function findHomologies(dataset) {
+    var scatterplot = [];
+    var genomeList = document.getElementsByName("genomeSelect");
+    for (var i = 0; i < genomeList.length; i++) {
+        if (genomeList[i].checked) {
+            console.log(genomeList[i].value);
+        }
+        else {
+            console.log("not checked");
+        }
+    }
+    return scatterplot;
 }
