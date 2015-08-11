de6d8fae2e0e7a421b3406c5f65bdb999551213d
sabine <sabine@sabine-virtual-machine>
2015-08-11 16:17:28 +0200

    Added example to start

---
+++ Syntheny_Scatterplot_SH_JS_RG/public_html/js/readStuff.js
@@ -0,0 +1,55 @@
+/* 
+ * Creates a list with each uploaded genome. Checkboxes allow to select them for
+ * comparison.
+ * Buttons: Reset, Submit, Upload
+ * 
+ */
+
+/*   
+ var dataset = [];
+ window.onload = function () { // on start and new upload
+ dataset = readJSON(dataset);
+ }; 
+ */
+
+d3.json("files/example_5.json", function (error, dataset) {
+    if (error)
+        return console.warn(error); // TBD
+    var i = 0; // id/value genome
+    var genomeSelect = d3.select("div#list")
+            .append("form")
+            .append("fieldset");
+    // Building inputelements
+    genomeSelect.selectAll("p")
+            .data(dataset)
+            .enter()
+            .append("p")
+            .append("label")
+            .append("input")
+            .attr("type", "checkbox")
+            .attr("name", "genomeSelect")
+//            .attr("checked", "true")  // for draft
+            .attr("value", function () {
+                return i++;
+            });
+    // Text of label after inputelement
+    genomeSelect.selectAll("label")
+            .append("text")
+            .text(function (d) {
+                return d.Name;
+            });
+    // Setting Buttons
+    genomeSelect.append("button")
+            .attr("type", "reset")
+            .text("Reset");
+    genomeSelect.append("button")
+            .attr("type", "button")
+            .attr("onclick", "findHomologies()")
+            .text("Submit");
+    genomeSelect.append("button")
+            .attr("type", "button")
+            .attr("disabled", "true")
+            // .attr("onclick", "uploadGenome()")
+            // .attr("formtarget", "_blank")
+            .text("Upload");
+});
